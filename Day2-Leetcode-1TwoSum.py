7c6ef245e01463e46a0f829cf2bfb82c1e2a3af0
Seokwon <73082542+seok10code@users.noreply.github.com>
2021-02-19 17:15:02 +0900

    Update Day2-Leetcode-1TwoSum.py

--- Day2-Leetcode-1TwoSum.py
+++ Day2-Leetcode-1TwoSum.py
@@ -1,12 +1,10 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-      left, right = 0, len(nums)-1
-      while not left == right:
-        if nums[left] + nums[right] < target:
-          left +=1
-        elif nums[left] + nums[right] > target:
-          right -=1
-        else:
-          return left, right
+        numsDict = {}
+    
+        for i, num in enumerate(nums):
+            if target - num in numsDict:
+                return [numsDict[target - num], i]
+            numsDict[num] = i
     
     
